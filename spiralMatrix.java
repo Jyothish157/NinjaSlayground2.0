a4df84232fc6a74395433b86e983b23e026da14e
Jyothishkumar V S <128952185+Jyothish157@users.noreply.github.com>
2024-12-06 05:27:39 +0530

    Create spiralMatrix.java

---
+++ spiralMatrix.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class spiralMatrix {public static int[] spiralMatrix(int[][] MATRIX) {
+        int rows = MATRIX.length;
+        int cols = MATRIX[0].length;
+        int[] result = new int[rows * cols];
+        int index = 0;
+        int top = 0, bottom = rows - 1, left = 0, right = cols - 1;
+
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                result[index++] = MATRIX[top][i];
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                result[index++] = MATRIX[i][right];
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    result[index++] = MATRIX[bottom][i];
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result[index++] = MATRIX[i][left];
+                }
+                left++;
+            }
+        }
+
+        return result;
+    }
+}
